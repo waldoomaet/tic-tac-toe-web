9933ed97aa2134ecddff8f171c6991108a07d3a3
Waldo Ruiz <waldo.omaet6446@gmail.com>
2023-10-11 03:20:54 +0200

    ws error handler added

--- src/App.vue
+++ src/App.vue
@@ -6,24 +6,24 @@
       <button type="button" @click="connectToServer" :disabled="host.length < 0 || port.length < 0">Connect</button>
     </div>
     <div class="row">
-      <div class="col"><button type="button" :disabled="disableButton(0, 0)"
-          @click="() => onClick(0, 0)">{{ board[0][0] }}</button></div>
-      <div class="col"><button type="button" :disabled="disableButton(0, 1)"
-          @click="() => onClick(0, 1)">{{ board[0][1] }}</button></div>
-      <div class="col"><button type="button" :disabled="disableButton(0, 2)"
-          @click="() => onClick(0, 2)">{{ board[0][2] }}</button></div>
+      <div class="col"><button type="button" :disabled="disableButton(0, 0)" @click="() => onClick(0, 0)">{{ board[0][0]
+      }}</button></div>
+      <div class="col"><button type="button" :disabled="disableButton(0, 1)" @click="() => onClick(0, 1)">{{ board[0][1]
+      }}</button></div>
+      <div class="col"><button type="button" :disabled="disableButton(0, 2)" @click="() => onClick(0, 2)">{{ board[0][2]
+      }}</button></div>
     </div>
     <div class="row">
-      <div class="col"><button type="button" :disabled="disableButton(1, 0)"
-          @click="() => onClick(1, 0)">{{ board[1][0] }}</button></div>
+      <div class="col"><button type="button" :disabled="disableButton(1, 0)" @click="() => onClick(1, 0)">{{ board[1][0]
+      }}</button></div>
       <div class="col"><button type="button" :disabled="disableButton(1, 1)" @click="() => onClick(1, 1)">{{
         board[1][1] }}</button></div>
       <div class="col"><button type="button" :disabled="disableButton(1, 2)" @click="() => onClick(1, 2)">{{
         board[1][2] }}</button></div>
     </div>
     <div class="row">
-      <div class="col"><button type="button" :disabled="disableButton(2, 0)"
-          @click="() => onClick(2, 0)">{{ board[2][0] }}</button></div>
+      <div class="col"><button type="button" :disabled="disableButton(2, 0)" @click="() => onClick(2, 0)">{{ board[2][0]
+      }}</button></div>
       <div class="col"><button type="button" :disabled="disableButton(2, 1)" @click="() => onClick(2, 1)">{{
         board[2][1] }}</button></div>
       <div class="col"><button type="button" :disabled="disableButton(2, 2)" @click="() => onClick(2, 2)">{{
@@ -146,54 +146,53 @@ export default {
       }
     },
     connectToServer() {
-      try {
-        this.socket = new WebSocket(`ws://${this.host}:${this.port}`);
-        this.socket.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          switch (data.status) {
-            case Status.connected:
-              if (data.id == 0) {
-                this.player = Game.x;
-                this.myTurn = true;
-              } else {
-                this.player = Game.o;
-                this.myTurn = false;
-              }
-              this.message = `Connected as player ${this.player}`;
-              break;
-            case Status.started:
-              this.resetGame();
-              if (this.player == Game.x) {
-                this.message = `Connected as player ${this.player}. You're first!`;
-              } else {
-                this.message = `Connected as player ${this.player}. Player x is first!`;
-              }
-              this.stopGame = false;
-              this.disableAll = false;
-              break;
-            case Status.turnChange:
-              this.board = data.board;
-              this.myTurn = true;
-              this.message = "It's your turn!"
-              this.disableAll = false;
-              break;
-            case Status.lose:
-              this.stopGame = true;
-              this.message = "Well... This is ackward... You lost...";
-              this.gameOver = true;
-              break;
-            case Status.draw:
-              this.stopGame = true;
-              this.message = "It's a draw!";
-              break;
-            case Status.playAgain:
-              this.resetGame();
-              break;
-          }
-        };
-      } catch (error) {
-        this.message = error;
+      this.socket = new WebSocket(`ws://${this.host}:${this.port}`);
+      this.socket.onerror = () => {
+        this.message = `Can't connect to ws://${this.host}:${this.port}`;
       }
+      this.socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        switch (data.status) {
+          case Status.connected:
+            if (data.id == 0) {
+              this.player = Game.x;
+              this.myTurn = true;
+            } else {
+              this.player = Game.o;
+              this.myTurn = false;
+            }
+            this.message = `Connected as player ${this.player}`;
+            break;
+          case Status.started:
+            this.resetGame();
+            if (this.player == Game.x) {
+              this.message = `Connected as player ${this.player}. You're first!`;
+            } else {
+              this.message = `Connected as player ${this.player}. Player x is first!`;
+            }
+            this.stopGame = false;
+            this.disableAll = false;
+            break;
+          case Status.turnChange:
+            this.board = data.board;
+            this.myTurn = true;
+            this.message = "It's your turn!"
+            this.disableAll = false;
+            break;
+          case Status.lose:
+            this.stopGame = true;
+            this.message = "Well... This is ackward... You lost...";
+            this.gameOver = true;
+            break;
+          case Status.draw:
+            this.stopGame = true;
+            this.message = "It's a draw!";
+            break;
+          case Status.playAgain:
+            this.resetGame();
+            break;
+        }
+      };
     }
   },
   mounted() {
@@ -210,4 +209,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}</style>
+}
+</style>
