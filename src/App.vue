afb08e8c7e29f67a420e1c906d2e524c80752e8b
arina_sbz <arinasbz@gmail.com>
2023-10-14 19:40:46 +0200

    minor changes in the UI

--- src/App.vue
+++ src/App.vue
@@ -18,8 +18,12 @@
       </div>
 
       <div class="d-flex">
-        <button class="connect-btn mt-2 p-2" type="button" @click="connectToServer"
-          :disabled="host.length < 0 || port.length < 0">
+        <button
+          class="primary-btn mt-2 p-2"
+          type="button"
+          @click="connectToServer"
+          :disabled="host.length < 0 || port.length < 0 || twoPlayers"
+        >
           <b>Connect</b>
         </button>
       </div>
@@ -28,25 +32,22 @@
           <svg
             v-if="wonIcon"
             xmlns="http://www.w3.org/2000/svg"
-            width="40"
-            height="40"
-            fill="#004d4d"
-            class="bi bi-emoji-wink"
-            viewBox="0 0 20 20"
+            width="35"
+            height="35"
+            fill="#00cc7a"
+            class="bi bi-emoji-wink-fill"
+            viewBox="0 0 16 16"
           >
             <path
-              d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
-            />
-            <path
-              d="M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683zM7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5zm1.757-.437a.5.5 0 0 1 .68.194.934.934 0 0 0 .813.493c.339 0 .645-.19.813-.493a.5.5 0 1 1 .874.486A1.934 1.934 0 0 1 10.25 7.75c-.73 0-1.356-.412-1.687-1.007a.5.5 0 0 1 .194-.68z"
+              d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM7 6.5C7 5.672 6.552 5 6 5s-1 .672-1 1.5S5.448 8 6 8s1-.672 1-1.5zM4.285 9.567a.5.5 0 0 0-.183.683A4.498 4.498 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.498 3.498 0 0 1 8 11.5a3.498 3.498 0 0 1-3.032-1.75.5.5 0 0 0-.683-.183zm5.152-3.31a.5.5 0 0 0-.874.486c.33.595.958 1.007 1.687 1.007.73 0 1.356-.412 1.687-1.007a.5.5 0 0 0-.874-.486.934.934 0 0 1-.813.493.934.934 0 0 1-.813-.493z"
             />
           </svg>
           <svg
-          v-else-if="lostIcon"
+            v-else-if="lostIcon"
             xmlns="http://www.w3.org/2000/svg"
             width="40"
             height="40"
-            fill="#cc0000"
+            fill="#ff1a1a"
             class="bi bi-emoji-frown"
             viewBox="0 0 20 20"
           >
@@ -56,58 +57,97 @@
           </svg>
           {{ message }}</b
         >
-        <button v-if="gameOver" type="button" @click="playAgain">
-          Play again?
+        <div class="d-flex justify-content-center">
+        <button v-if="gameOver" class="primary-btn p-2" type="button" @click="playAgain">
+          <b>Click to play again!</b>
+          
         </button>
+      </div>
       </div>
       <div class="game-board p-5" v-if="twoPlayers">
         <div class="row board-row">
           <div class="col board-item border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(0, 0)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(0, 0)"
+            >
               {{ board[0][0] }}
             </button>
           </div>
           <div class="col board-item border-left border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(0, 1)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(0, 1)"
+            >
               {{ board[0][1] }}
             </button>
           </div>
           <div class="col board-item border-left border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(0, 2)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(0, 2)"
+            >
               {{ board[0][2] }}
             </button>
           </div>
         </div>
         <div class="row board-row">
           <div class="col board-item border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(1, 0)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(1, 0)"
+            >
               {{ board[1][0] }}
             </button>
           </div>
           <div class="col board-item border-left border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(1, 1)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(1, 1)"
+            >
               {{ board[1][1] }}
             </button>
           </div>
           <div class="col board-item border-left border-bottom">
-            <button class="board-btn" type="button" @click="() => onClick(1, 2)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(1, 2)"
+            >
               {{ board[1][2] }}
             </button>
           </div>
         </div>
         <div class="row board-row">
           <div class="col board-item">
-            <button class="board-btn" type="button" @click="() => onClick(2, 0)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(2, 0)"
+            >
               {{ board[2][0] }}
             </button>
           </div>
           <div class="col board-item border-left">
-            <button class="board-btn" type="button" @click="() => onClick(2, 1)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(2, 1)"
+            >
               {{ board[2][1] }}
             </button>
           </div>
           <div class="col board-item border-left">
-            <button class="board-btn" type="button" @click="() => onClick(2, 2)">
+            <button
+              class="board-btn"
+              type="button"
+              @click="() => onClick(2, 2)"
+            >
               {{ board[2][2] }}
             </button>
           </div>
@@ -222,7 +262,7 @@ export default {
               board: this.board,
             })
           );
-          this.message = `You won!`;
+          this.message = `You won! Yaaayyyy!`;
           this.wonIcon = true;
           this.gameOver = true;
           return;
@@ -280,7 +320,7 @@ export default {
             break;
           case Status.lose:
             this.stopGame = true;
-            this.message = "Well... This is ackward... You lost...";
+            this.message = "Well... This is awkward... You lost...";
             this.lostIcon = true;
             this.gameOver = true;
             break;
@@ -315,14 +355,14 @@ export default {
   height: 100vh;
   background-image: linear-gradient(to top, #30cfd0 0%, #330867 100%);
 
-  .connect-btn {
+  .primary-btn {
     border: none;
     border-radius: 10px;
     width: 12rem;
     box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
       rgba(0, 0, 0, 0.3) 0px 7px 13px -3px,
       rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-    &:hover {
+    &:hover:enabled {
       transform: translateY(-2px);
       transition: all 0.2s;
       background-image: linear-gradient(
@@ -362,11 +402,11 @@ export default {
   }
 
   .border-left {
-    border-left: 2px solid #fff;
+    border-left: 4px solid #fff;
   }
 
   .border-bottom {
-    border-bottom: 2px solid #fff !important;
+    border-bottom: 4px solid #fff !important;
   }
 }
 </style>
